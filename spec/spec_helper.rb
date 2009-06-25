bcb070cec61742e26a6081e1f2151cfa959f26b2
Jim Remsik and Sandro Turriate <dev+bigtiger+sandro@hashrocket.com>
2009-06-25 17:13:00 -0400

    spec helper fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-require 'spec'
-
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rolex'
+require 'spec'
+require 'spec/autorun'
 
 Spec::Runner.configure do |config|
   
