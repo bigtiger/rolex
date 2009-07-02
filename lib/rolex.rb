641222a742bb296b95a2cbd3447c5bf2699f1610
Sandro Turriate <sandro.turriate@gmail.com>
2009-07-03 03:56:38 +0800

    Run a report for any given date
    - Refactored #today to use the new #report_for method
    
    Signed-off-by: Jim Remsik <bigtiger@hashrocket.com>

--- lib/rolex.rb
+++ lib/rolex.rb
@@ -3,11 +3,13 @@ $:.unshift(File.dirname(__FILE__))
 begin; require 'rubygems'; rescue LoadError; end
 require 'grit'
 require 'float_ext'
+require 'time_ext'
 
 class Rolex
   autoload :Report, 'rolex/report'
   autoload :LineItem, 'rolex/line_item'
-  
+
+  attr_reader :repo
   attr_accessor :who
 
   def initialize(repo_location, who)
@@ -22,10 +24,8 @@ class Rolex
   end
 
   def today
-    commits = Grit::Commit.find_all(@repo, nil, :since => Time.today)
-    Report.new(commits, who).run
+    report_for Date.today
   end
-end
 
   private
 
--- spec/rolex_spec.rb
+++ spec/rolex_spec.rb
@@ -3,24 +3,35 @@ require File.join(File.dirname(__FILE__),'spec_helper')
 describe "Rolex" do
   before do
     @commits = ["commit1", "commit2"]
-    @repo = Grit::Repo.stub!(:new).and_return('/some/path')
-    @rolex = Rolex.new(@repo, "someuser")
+    @rolex = Rolex.new('.', "someuser")
     @report = stub('report', :run => true)
+    @now = Time.now
   end
 
-  describe '#today' do 
-    it "retrieves today's commits" do
-      Grit::Commit.should_receive(:find_all).with('/some/path', nil, :since => anything).and_return(@commits)
-      Rolex::Report.should_receive(:new).with(@commits, "someuser").and_return(@report)
-      @rolex.today
+  describe "#report_for" do
+    it "parses strings with Chronic" do
+      Chronic.should_receive(:parse).with("yesterday").and_return(Time.now)
+      @rolex.report_for("yesterday")
     end
-    
+
+    it "retrieves commits" do
+      @rolex.repo.should_receive(:commits_since).with(nil, @now.to_date, :until => @now.to_date + 1, :author => @rolex.who).and_return(@commits)
+      Rolex::Report.should_receive(:new).with(@commits).and_return(@report)
+      @rolex.report_for(@now)
+    end
+
     it "runs a report" do
-      Grit::Commit.should_receive(:find_all).and_return(@commits)
+      @rolex.repo.stub!(:commits_since => @commits)
       Rolex::Report.stub!(:new).and_return(@report)
       @report.should_receive(:run)
       @rolex.today
     end
   end
 
+  it "finds the commits for the given time period" do
+
+  end
+
+  describe '#today' do 
+  end
 end
